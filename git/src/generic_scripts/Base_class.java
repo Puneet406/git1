3ad818ecae90c834d4ce3c644a911df8a8cc2f4e
user <user@user-PC>
2022-12-11 18:54:44 +0530

    hi

---
+++ git/src/generic_scripts/Base_class.java
@@ -0,0 +1,33 @@
+package generic_scripts;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Base_class implements Framework {
+     
+	   public WebDriver driver;
+	   @BeforeMethod
+	   public void open()
+	   {
+		   System.setProperty(key, value);
+		   driver=new FirefoxDriver();
+		   driver.get(url);
+		   
+	   }
+	   @AfterMethod
+	   public void close(ITestResult result) throws IOException
+	   {
+		   if(ITestResult.FAILURE==result.getStatus())
+		   {
+			   photo.pic(driver);
+		   }
+		  
+			   driver.close();
+		   
+	   }
+}
